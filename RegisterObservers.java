1d6d64b5c6952a4c43ecc666f3f24b3e7d1bd68f
Shihab Abrar <shihababrar99@gmail.com>
2019-04-26 22:01:18 +0600

    "s5"

---
+++ RegisterObservers.java
@@ -0,0 +1,33 @@
+package com.example.e_boimela;
+
+public class RegisterObservers {
+
+    private String observerEmailAddress;
+    private String observerStallName;
+    private String observerStallNumber;
+
+    public RegisterObservers(){
+        //
+    }
+
+    public RegisterObservers(String observerEmailAddress, String observerStallName, String observerStallNumber) {
+        this.observerEmailAddress = observerEmailAddress;
+        this.observerStallName = observerStallName;
+        this.observerStallNumber = observerStallNumber;
+    }
+
+    public String getObserverEmailAddress() {
+        return observerEmailAddress;
+    }
+
+
+    public String getObserverStallName() {
+        return observerStallName;
+    }
+
+
+    public String getObserverStallNumber() {
+        return observerStallNumber;
+    }
+
+}
